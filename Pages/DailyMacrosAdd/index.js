0831e642645cc9e0cd1954befe1c51eca2220605
juliano <julianopaulo.santos@hotmail.com>
2024-03-12 22:26:32 -0300

    add react native select picker, list created food

--- Pages/DailyMacrosAdd/index.js
+++ Pages/DailyMacrosAdd/index.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import DefaultInputText from '../../Components/DefaultInputText'
 import CustomRadioButton from '../../Components/CustomRadioButton'
 import DefaultButton from '../../Components/DefaultButton';
+import { addDailyMacros } from '../../services/addOrEditCollection';
+import { Picker } from '@react-native-picker/picker';
+import { listFood } from '../../services/getDataFromCollection'
+import { useIsFocused } from '@react-navigation/core';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,31 +17,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  select: {
+    borderColor: 'rgba(40, 60, 80, 1)',
+    borderWidth: 1,
+    height: 40,
+    justifyContent: 'center',
+    borderRadius: 5,
+    margin: 12
+  }
 });
 
 function DailyMacrosAdd() {
+  const isFocused = useIsFocused();
+
+  const [foodList, setFoodList] = useState([]);
   const [portion, setPortion] = useState('0');
   const [portionType, setPortionType] = useState('g');
+  const [selectedFood, setSelectedFood] = useState({});
+
+  const loadData = async () => {
+    setFoodList(await listFood());
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [isFocused]);
 
   const handleSubmit = async () => {
     data = {
-      name,
       portion,
       portionType,
-      calories,
-      proteins,
-      carbos,
-      totalFat,
-      saturatedFat,
-      monounsaturatedFat,
-      polyunsaturatedFat,
-      transFat,
-      dietaryFiber,
-      sodium,
+      selectedFood
     };
 
     try {
-      // await addFoodMacros(data);
+      // await addDailyMacros(data);
       //display success message
       // Alert.alert(`Alimento ${name} cadastrado com sucesso!`, 'Tudo certo, pode cadastrar o próximo!', [
       //   {
@@ -46,8 +60,9 @@ function DailyMacrosAdd() {
       //   },
       // ]);
 
-      setPortion('0')
-      setPortionType('g')
+      setPortion('0');
+      setPortionType('g');
+      setSelectedFood({});
     } catch (error) {
       console.log('error', error.message);
       Alert.alert(`Erro ao cadastrar o refeição!`, 'Ops, verifique os campos e tente novamente!', [
@@ -62,17 +77,28 @@ function DailyMacrosAdd() {
   return (
     <View style={styles.container}>
       <Text>Cadastro de refeição</Text>
+      <View style={styles.select}>
+        <Picker
+          style={{width: 300}}
+          selectedValue={selectedFood}
+          onValueChange={(itemValue) => setSelectedFood(itemValue)}
+        >
+            {foodList.map((food) => (
+              <Picker.Item label={food.name} value={food} key={food.id} />
+            ))}
+        </Picker>
+      </View>
       <View
         style={{
           display: 'flex',
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'space-between',
-          width: 200
+          width: 300
         }}>
         <DefaultInputText
           placeholder='Porção'
-          inputWitdh={100}
+          inputWitdh={200}
           keyboardType='numeric'
           margin={0}
           onChange={e => setPortion(e)}
